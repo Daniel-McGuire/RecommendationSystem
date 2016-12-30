fe46760d98b15c9493de337b621f00dcecefea78
Rhiannon Ayres <rhiannon.ayres@outlook.com>
2016-12-30 16:06:32 +0000

    updated

--- JavaScript.js
+++ JavaScript.js
@@ -7,10 +7,10 @@ alert("Hello world");
 //changes the colour of the buttons if they are checked. 
 function checkBoxSelection(CheckboxID, LblID) { 
 	var labelID = document.getElementById(LblID); 
+	
 	if(document.getElementById(CheckboxID).checked == true) {
-	labelID.style.backgroundColor= "#0489B1";
+	labelID.style.backgroundColor= "#789E9E";
 	labelID.style.color= "white";
-	//alert(CheckboxID + LblID + " is checked");
 	}
 	else {
 		labelID.style.backgroundColor= "#cfe2e2";
