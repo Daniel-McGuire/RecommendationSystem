054c75a57c71c44431f8a190bd150699f08cea87
Rhiannon Ayres <rhiannon.ayres@outlook.com>
2017-01-12 14:05:01 +0000

    added SPARQL query for museums to test. Need to display results in a table and need to have multiple markers on map

--- js/js.js
+++ js/js.js
@@ -30,52 +30,67 @@ var btn = document.getElementById("search-btn");
 
 btn.addEventListener("click", function() { //when button is clicked...
 
-var url = "http://dbpedia.org/sparql";
+	var url = "http://dbpedia.org/sparql";
 
-var query = [  //save SPARQL query in JavaScript variable called 'query'
+	var query = [  //save SPARQL query in JavaScript variable called 'query'
+	
+	"SELECT DISTINCT ?location ?museums MAX(?lat) as ?lat MAX(?long) as ?long",
+	"WHERE {",
+	"?m rdf:type dbo:Museum.",
+	"?l dbo:location ?x.",
+	"?x dbo:country dbr:United_Kingdom",
+	"FILTER regex(?location, \"" + document.getElementById("search-box").value + "\" ,\"i\")",  
+	"?m dbo:location ?x;",
+    "geo:lat ?lat;",
+	"geo:long ?long.",
+	"?m rdfs:label ?museums.",
+	"FILTER langMatches( lang(?museums), \"EN\" )",
+	"?x rdfs:label ?location.",
+	"FILTER langMatches( lang(?location), \"EN\" )",
+	"}",
+	].join(" ");
 
-"PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>",
-"PREFIX type: <http://dbpedia.org/class/yago/>",
-"PREFIX prop: <http://dbpedia.org/property/>",
+	//create variable for the URI of the query called 'queryURl'
+	var queryUrl= url+"?query="+ encodeURIComponent(query) +"&format=json";
+	var HTTPRequest = new XMLHttpRequest();
 
-"SELECT ?lat ?long ?location",
-"WHERE{",
-"?a dbo:location ?location;",
-"geo:lat ?lat;",
-"geo:long ?long.",
-"?location dbo:country dbr:United_Kingdom.",
-"FILTER regex(?location, \"" + document.getElementById("search-box").value + "\" ,\"i\")",  // same as e.g. ... "FILTER regex(?city, "Cardiff", "i")",
-"}LIMIT 1",
-].join(" ");
+	HTTPRequest.open('GET', queryUrl); //'POST' = send data 'GET' = receive
+		HTTPRequest.onload = function(){
+		console.log(HTTPRequest.responseText); //prints the JSON to the console
+		var JSONData = JSON.parse(HTTPRequest.responseText);
+		getLatLong(JSONData);
+		};
 
-//create variable for the URI of the query called 'queryURl'
-var queryUrl= url+"?query="+ encodeURIComponent(query) +"&format=json";
-var HTTPRequest = new XMLHttpRequest();
-
- HTTPRequest.open('GET', queryUrl); //'POST' = send data 'GET' = receive
- HTTPRequest.onload = function(){
-	console.log(HTTPRequest.responseText); //prints the JSON to the console
-	var JSONData = JSON.parse(HTTPRequest.responseText);
-	getLatLong(JSONData);
-};
-
-HTTPRequest.send();
+	HTTPRequest.send();
 
 });
 
 function getLatLong(data){
 	//loop through JSON to get results
+	var table = tbl  = document.createElement('table');
+	
+	var attractionType = data.head.vars[1];
+	var locationHeader = data.head.vars[0];
 
 	for (i = 0; i< data.results.bindings.length; i++){
 			var latitude =  parseFloat(data.results.bindings[i].lat.value);
 			var longitude = parseFloat(data.results.bindings[i].long.value);
+			var attraction = data.results.bindings[i].museums.value;
 			var location = data.results.bindings[i].location.value;
-		}
+			
 
 	//plot latitude and longitude on map
-
+	
 	drawMap(latitude, longitude, location);
+    resultsPanel.innerHTML += "<br />" + attractionType + "<br />" + " Location: " + location + "  Name: " + attraction;
+	
+	}
+	
 }
+
+
+
+
 
 function drawMap(latValue, longValue, location) {
 		var map;
@@ -89,7 +104,7 @@ function drawMap(latValue, longValue, location) {
 		var marker = new google.maps.Marker({
           position: myLatLong,
           map: map,
-          title: location 
+		  title: location
 		  
       });
 	  
