c41d601f45fb6cd5009cf6095abea221fefa05f0
Rhiannon Ayres <rhiannon.ayres@outlook.com>
2017-01-20 11:42:32 +0000

    updated PHP/getQuery.php based on new DB entry

--- PHP/getQuery.php
+++ PHP/getQuery.php
@@ -1,22 +1,37 @@
 <?php
-require_once('C:\xampp\htdocs\PHP\connectSQL.php'); //connect to database
 
-$query = "SELECT query FROM queries WHERE Criteria = 'testingtesting'"; //get SPARQL query from queries table
+$criteria = trim($_GET['criteria']); 
+$location = trim($_GET['location']); 
+
+require_once('../PHP/connectSQL.php'); //connect to database
+
+$query = "SELECT DISTINCT results, location, lat, longi, mapMarkers 
+		  FROM sparqldb.sparql_results2 
+          WHERE 
+		  (criteria = '".$criteria."') AND
+		  (location = '".$location."') AND
+		  (date_entered > NOW() - INTERVAL 2 HOUR)"; //date entered within the last 2 hours
 
 $response = @mysqli_query($dbc, $query); 
 
-if($response){
-	while($row = mysqli_fetch_array($response)){
-       echo
-       $row['query']; 
+$rows = array();
+
+if($response->num_rows > 0) {
+	
+	while($r = mysqli_fetch_assoc($response)){
+       $rows[] = $r;
       } 
-}
-else {
-	echo "couldn't issue database query";
-	echo mysqli_error($dbc);
+	  
+	  echo json_encode($rows);
 }
 
-mysqli_close($dbc);
+if($response->num_rows == 0){
+	echo '0';
+}
+
+
+
+mysqli_close($dbc); //close database connection
 ?>
 
 
